aefcec44abcaeeb0183969926872368a061e7313
miljana5kovic <miljana.b.petkovic@gmail.com>
2025-04-06 17:13:31 +0200

    Sidebar basic structure

--- spiralist/src/app/components/sidebar/sidebar.component.ts
+++ spiralist/src/app/components/sidebar/sidebar.component.ts
@@ -1,9 +1,11 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { MatIconModule } from '@angular/material/icon';
+import { PropDefinerComponent } from '../prop-definer/prop-definer.component';
 
 @Component({
   selector: 'sidebar',
-  imports: [CommonModule],
+  imports: [CommonModule, MatIconModule, PropDefinerComponent],
   templateUrl: './sidebar.component.html',
   styleUrl: './sidebar.component.scss'
 })
