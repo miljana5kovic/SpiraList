8a6151501b37ca7ec6d82a146b43cc9296d847b1
miljana5kovic <miljana.b.petkovic@gmail.com>
2025-04-23 21:17:26 +0200

    Revert "Live fixes"
    This reverts commit 9eb3436e336035f97e2ba3daf77de6ac074a5936.

--- spiralist/docs/server/angular-app-manifest.mjs
+++ spiralist/docs/server/angular-app-manifest.mjs
@@ -14,7 +14,7 @@ export default {
   assets: {
     'index.csr.html': {size: 23573, hash: '568f44529e71eeeb87770d231e97cd8c58cff9822aef62b105e000618c5b6bd8', text: () => import('./assets-chunks/index_csr_html.mjs').then(m => m.default)},
     'index.server.html': {size: 17147, hash: '4c0e4573247a8f9e3c2d5cfa8d176b15f43ae6f132e02983536e0c54359e3c03', text: () => import('./assets-chunks/index_server_html.mjs').then(m => m.default)},
-    'index.html': {size: 83933, hash: '46e3d12fc777da3195c831a19a8748a27c46ab50a9c4a878086d75a7f3b0cf4d', text: () => import('./assets-chunks/index_html.mjs').then(m => m.default)},
+    'index.html': {size: 83933, hash: '11ad340bfe1e3395894b645911f148c344683da97f72a307fa0bebc9d4838452', text: () => import('./assets-chunks/index_html.mjs').then(m => m.default)},
     'styles-36AW6TKX.css': {size: 6979, hash: 'vY6tjD/ce7M', text: () => import('./assets-chunks/styles-36AW6TKX_css.mjs').then(m => m.default)}
   },
 };
